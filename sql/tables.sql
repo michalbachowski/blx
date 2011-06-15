b6f29357971e9b8251746761606a2df3a3460d8e
Michał - MiB - Bachowski <mib@heroes.net.pl>
2011-06-15 15:36:36 +0200

    Added first version of SQL that generates table for database based storage

---
+++ sql/tables.sql
@@ -0,0 +1,8 @@
+create table pages (
+    url varchar primary key,
+    realm varchar not null,
+    title varchar not null,
+    content text not null,
+    created timestamp without time zone not null default now(),
+    user_id integer not null
+);
