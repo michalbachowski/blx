20306f2a3b6663e7973780de3224a1c9bed5c9e2
Michał - MiB - Bachowski <mib@heroes.net.pl>
2011-06-15 15:20:13 +0200

    Added plugn that loads files from given directory

---
+++ plugins/FileFromDirectory.php
@@ -0,0 +1,31 @@
+<?php
+namespace Blx\Plugin;
+
+class FileFromDirectory {
+    protected $directory;
+
+    public function __construct( $dir ) {
+        $this->directory = $dir;
+    }
+    public function update( \sfEvent $event ) {
+        if ( !is_dir( $this->directory ) ) {
+            return;
+        }
+        $path = realpath(
+            $this->directory .
+            DIRECTORY_SEPARATOR .
+            ltrim(
+                $event['url'],
+                DIRECTORY_SEPARATOR
+            )
+        );
+        if ( strpos( $path, $this->directory ) !== 0 ) {
+            return;
+        }
+        if ( !file_exists( $path ) ) {
+            return;
+        }
+        $event->setReturnValue( file_get_contents( $path ) );
+        return true;
+    }
+}
