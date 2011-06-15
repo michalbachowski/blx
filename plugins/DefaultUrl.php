49ca7109481b4e49e1dae446debc26a50c2bb7b4
Michał - MiB - Bachowski <mib@heroes.net.pl>
2011-06-15 15:19:51 +0200

    Added plugin that sets default URL in case of empty URL

---
+++ plugins/DefaultUrl.php
@@ -0,0 +1,16 @@
+<?php
+namespace Blx\Plugin;
+
+class DefaultUrl {
+    protected $url;
+
+    public function __construct( $url ) {
+        $this->url = $url;
+    }
+    public function filter( \sfEvent $event, $value ) {
+        if ( empty( $value ) ) {
+            $value = $this->url;
+        }
+        return $value;
+    }
+}
