5e0b7a893ccf1aac4a542b52c865d7afd8e7464b
Michał - MiB - Bachowski <mib@heroes.net.pl>
2011-06-16 15:48:57 +0200

    Allow metadata to be empty

--- sql/tables.sql
+++ sql/tables.sql
@@ -2,7 +2,7 @@ create table pages (
     url varchar primary key,
     title varchar not null,
     content text not null,
-    medatada text not null,
+    medatada text,
     created timestamp without time zone not null default now(),
 );
 
