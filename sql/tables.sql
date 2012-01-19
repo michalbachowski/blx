447b3b4207f3d8ac880cf2ea4e58a176757b3228
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-01-19 11:00:45 +0100

    Changes to table schema (added search_id and creation date for trace entries)

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,14 +1,20 @@
+/*Sequence pages_search_id */
+create sequence pages_search_id_seq increment by 1 minvalue 1 no maxvalue start with 1 no cycle;
+
 create table pages (
     url varchar primary key,
     realm varchar not null,
     title varchar not null,
     content text not null,
     metadata text,
-    created timestamp without time zone not null default now()
+    created timestamp without time zone not null default now(),
+    search_id integer not null default nextval( 'pages_search_id_seq' ) unique
 );
 
 create table pages_trace (
     action char(1) not null,
+    created timestamp without time zone not null default now(),
     dbuser text NOT NULL,
-    like pages including defaults
+    constraint pages_trace_pkey primary key ( url, created ),
+    like files
 );
--- sql/triggers.sql
+++ sql/triggers.sql
@@ -2,13 +2,13 @@ create or replace function tr_pages_trace()
 returns TRIGGER language 'plpgsql' security definer as $$
 begin
     IF 'DELETE' = TG_OP THEN
-        INSERT INTO blx.pages_trace SELECT 'D', user, OLD.*;
+        INSERT INTO blx.pages_trace SELECT 'D', now(), user, OLD.*;
         RETURN OLD;
     ELSIF 'UPDATE' = TG_OP THEN
-        INSERT INTO blx.pages_trace SELECT 'U', user, NEW.*;
+        INSERT INTO blx.pages_trace SELECT 'U', now(), user, NEW.*;
         RETURN NEW;
     ELSIF 'INSERT' = TG_OP THEN
-        INSERT INTO blx.pages_trace SELECT 'I', user, NEW.*;
+        INSERT INTO blx.pages_trace SELECT 'I', now(), user, NEW.*;
         RETURN NEW;
     END IF;
     return null;
@@ -16,5 +16,5 @@ end;
 $$;
 
 CREATE TRIGGER trg_pages_trace
-AFTER INSERT on pages
+AFTER INSERT or UPDATE or DELETE on pages
     FOR EACH ROW EXECUTE PROCEDURE tr_pages_trace();
