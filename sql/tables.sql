eb2b84ec1cc65bb3d6e3de6f6b64bb71beaa2491
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-02-02 14:04:15 +0100

    Use realm within primary keys

--- sql/tables.sql
+++ sql/tables.sql
@@ -2,18 +2,19 @@
 create sequence pages_search_id_seq increment by 1 minvalue 1 no maxvalue start with 1 no cycle;
 
 create table pages (
-    url varchar primary key,
+    url varchar not null,
     realm varchar not null,
     title varchar not null,
     content text not null,
     metadata text,
-    search_id integer not null default nextval( 'pages_search_id_seq' ) unique
+    search_id integer not null default nextval( 'pages_search_id_seq' ) unique,
+    primary key (url, realm)
 );
 
 create table pages_trace (
     action char(1) not null,
     created timestamp without time zone not null default now(),
     dbuser text NOT NULL,
-    constraint pages_trace_pkey primary key ( url, created ),
+    constraint pages_trace_pkey primary key ( url, realm, created ),
     like pages
 );
