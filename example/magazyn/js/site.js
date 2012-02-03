14b9e00ee9fbf84f8f19ae91ad6a5e371973682d
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-02-03 19:27:26 +0100

    Improved generating URLs to new page and edit page

--- example/magazyn/js/site.js
+++ example/magazyn/js/site.js
@@ -2,12 +2,26 @@ $(document).ready(function() {
     jbUser( function() {
         jbManageBar( jbLang( "Add" ), function() {
             var url = window.prompt( jbLang( "Type new page url" ) );
-            if ( url ) {
-                window.location = url;
+            if ( !url ) {
+                return;
             }
+            if ( url.indexOf(".html") == - 1 && url.indexOf(".dhtml") == -1 ) {
+                url += '.dhtml?edit=1';
+            } else if ( url.indexOf(".dhtml") == - 1 ) {
+                url = url.replace(/\.html($|\?)/, ".dhtml$1?edit=1");
+            } else {
+                url += '?edit=1';
+            }
+            window.location = url;
         } );
-        if ( window.location.href.indexOf(".dhtml") == - 1 ) {
-            jbManageBar( jbLang( "Edit" ), window.location.href.replace(/\.html($|\?)/, ".dhtml$1") );
+        var url = window.location.protocol + '//' + window.location.host;
+        if ( window.location.pathname == '/' ) {
+            url += '/index.html';
+        } else {
+            url += window.location.pathname;
+        }
+        if ( url.indexOf(".dhtml") == - 1 ) {
+            jbManageBar( jbLang( "Edit" ), url.replace(/\.html($|\?)/, ".dhtml$1?edit=1") );
         }
     } );
 });
