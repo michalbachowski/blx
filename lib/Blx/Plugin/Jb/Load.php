4495207bf7af598e74af91d6ec644889ad928fc1
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-02-04 22:13:27 +0100

    Load Blx codeset and text domain

--- lib/Blx/Plugin/Jb/Load.php
+++ lib/Blx/Plugin/Jb/Load.php
@@ -12,10 +12,16 @@ class Load extends \Blx\Plugin {
     }
 
     public function update( \sfEvent $event ) {
+        // load core
         define( 'JB_REALM', $this->realm );
         define( 'JB_DEBUG', 1 );
         require 'jbcore/jbcore.php';
 
+        // load Blx translations
+    	bind_textdomain_codeset( JB_REALM, JB_ENCODING );
+	    bindtextdomain( JB_REALM, JB_LOCALE_DIR );
+
+        // fix url pattern
         $util = $event->getSubject()->getUtil();
         $util->setUrlPattern( \Url::make( $util->getUrlPattern() ) );
     }
