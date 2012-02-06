9ef006688fa870016e77b9e91776e3ebf78f9653
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-02-06 08:27:18 +0100

    Fixed Blx\Plugin\IncludeContent plugin; Added additional parsing hook to filter.output

--- lib/Blx/Plugin/IncludeContent.php
+++ lib/Blx/Plugin/IncludeContent.php
@@ -3,14 +3,15 @@ namespace Blx\Plugin;
 
 class IncludeContent extends \Blx\Plugin  {
     protected $mapping = array(
-        'filter.response.normal' => 'filter'
+        'filter.response.normal' => 'filter',   // filter content
+        'filter.output' => 'filter'             // filter layout
     );
     protected $pattern = '!\[include\:(.+?)\]!';
 
     public function filter( \sfEvent $event, $content ) {
         $callback = function( $matches ) use ( $event ) {
             return $event->getSubject()->loadContent( \Blx\Request::GET, $matches[1] );
-        }
+        };
         return preg_replace_callback(
             $this->pattern,
             $callback,
