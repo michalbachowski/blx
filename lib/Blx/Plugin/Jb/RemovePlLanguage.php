d5fad00b8db52e162b126375ec4807f32d37786e
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-02-07 10:14:37 +0100

    Enhanced Blx\Plugin\Jb\RemovePlLanguage to redirect to URL without 'pl/' when user entered such url

--- lib/Blx/Plugin/Jb/RemovePlLanguage.php
+++ lib/Blx/Plugin/Jb/RemovePlLanguage.php
@@ -5,10 +5,21 @@ class RemovePlLanguage extends \Blx\Plugin {
     protected $mapping = array(
         'filter.url' => 'fix'
     );
+    protected $baseUrl;
 
     public function fix( \sfEvent $event, $url ) {
-        if ( strpos( $url, 'pl/' ) === 0 ) {
-            return substr( $url, 3 );
+        $tmp = $this->_fix( $event, $url );
+        $this->baseUrl = $url;
+        return $tmp;
+    }
+
+    protected function _fix( \sfEvent $event, $url ) {
+        if ( strpos( $url, 'pl/' ) !== 0 ) {
+            return $url;
+        }
+        $url = substr( $url, 3 );
+        if ( null === $this->baseUrl ) {
+            $event->getSubject()->redirectToPage( $url );
         }
         return $url;
     }
