bab58b554eb217e4dcc48452644bbe3aa53f1cc0
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-02-07 16:29:29 +0100

    Added 'Upload' button

--- example/magazyn/js/site.js
+++ example/magazyn/js/site.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     jbUser( function( user ) {
         if ( user.id == -1 ) {
             return;
-        } 
+        }
+        // Add
         jbManageBar( jbLang( "Add" ), function() {
             var url = window.prompt( jbLang( "Type new page url" ) );
             if ( !url ) {
@@ -20,6 +21,7 @@ $(document).ready(function() {
             }
             window.location = url;
         } );
+        // Edit
         var url = window.location.protocol + '//' + window.location.host;
         if ( window.location.pathname == '/' ) {
             url += '/index.html';
@@ -29,8 +31,35 @@ $(document).ready(function() {
         if ( url.indexOf(".dhtml") == - 1 ) {
             jbManageBar( jbLang( "Edit" ), url.replace(/\.html($|\?)/, ".dhtml$1?edit=1") );
         }
+        // Special pages
         if ( url.indexOf("_special.html") == -1 ) {
             jbManageBar( jbLang( "Special pages" ), "/_special.html" );
         }
+        // Upload
+        jbManageBar( jbLang( "Upload" ), (function () {
+            var iframe;
+            var dialog;
+            return function() {
+                if ( typeof( iframe ) == "undefined" ) {
+                    iframe = $( "<iframe />" ).attr( "src", "http://kopalnia.heroes.net.pl/upload?format=light" );
+                    dialog = $( "<div />" )
+                        .attr( "id", "upload-form" )
+                        .append( iframe )
+                        .appendTo( "body" )
+                        .dialog({
+                            autoOpen: false,
+                            modal: true,
+                            title: jbLang( "Upload" ),
+                            height: 530,
+                            width: 500,
+                            beforeClose: function( event, ui ) {
+                                window.location.reload();
+                            }
+                        });
+                }
+                dialog.dialog( "open" );
+                return false;
+            };
+        })() );
     } );
 });
