77ee0ad09a58a8969f886bebda628396cfb34ab2
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-02-09 11:34:45 +0100

    Use ->request instead of ->getSubject()

--- lib/Blx/Plugin/IncludeContent.php
+++ lib/Blx/Plugin/IncludeContent.php
@@ -9,8 +9,9 @@ class IncludeContent extends \Blx\Plugin  {
     protected $pattern = '!\[include\:(.+?)\]!';
 
     public function filter( \sfEvent $event, $content ) {
-        $callback = function( $matches ) use ( $event ) {
-            return $event->getSubject()->loadContent( \Blx\Request::GET, $matches[1] );
+        $request = $this->request;
+        $callback = function( $matches ) use ( $request ) {
+            return $request->loadContent( \Blx\Request::GET, $matches[1] );
         };
         return preg_replace_callback(
             $this->pattern,
