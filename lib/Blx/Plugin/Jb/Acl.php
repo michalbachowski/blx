74e605dc3b62e08da3a2e2e1fcc696922a65e298
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-02-09 11:30:41 +0100

    Refactored Blx\Plugin\Jb\Acl to handle GET/POST requests

--- lib/Blx/Plugin/Jb/Acl.php
+++ lib/Blx/Plugin/Jb/Acl.php
@@ -7,27 +7,37 @@ class Acl extends \Blx\Plugin {
     );
     const ALLOW = true;
     const DENY = false;
-    protected $default;
+    protected $defaultResponse;
 
     public function __construct( $default = self::DENY ) {
-        $this->default = $default;
+        $this->defaultResponse = $default;
+        $this->mode = '.' . strtolower( $_SERVER['REQUEST_METHOD'] ?: 'get' );
+
     }
 
     public function filter( \sfEvent $event, $url ) {
-        $args = array( 'event' => $event, 'url' => $url );
-        $allowGlobal = \JBPerm::verify( $this, 'perm.' . JB_REALM, $args );
-        $allowPage = \JBPerm::verify( $this, 'perm.page.' . $url, $args );
-        // an event was verified positively
-        if ( $allowGlobal || $allowPage ) {
-            return $url;
-        }
-        // no event was processed (no policies defined) - use default value
-        if ( null === $allowGlobal && null === $allowPage && $this->default ) {
+        $response = $this->check( $event, $url );
+        // all following check will be always GET
+        $this->mode = '.get';
+        if ( $response ) {
             return $url;
         }
         // access denied
         throw new \Blx\ForbiddenError(
             _( 'You are not allowed to acces this page.' )
         );
+    }
+    protected function check( \sfEvent $event, $url ) {
+        $args = array( 'event' => $event, 'url' => $url );
+        $allowGlobal = \JBPerm::verify( $this, 'perm.' . JB_REALM . $this->mode, $args );
+        $allowPage = \JBPerm::verify( $this, 'perm.page.' . $url . $this->mode, $args );
+        // an event was verified positively
+        if ( ( $allowGlobal && $allowPage !== false ) || $allowPage ) {
+            return true;
+        }
+        // no event was processed (no policies defined) - use default value
+        if ( null === $allowGlobal && null === $allowPage && null !== $this->defaultResponse ) {
+            return true;
+        }
     }
 }
