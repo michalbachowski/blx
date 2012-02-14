848dff68189cf57e585f7f2ebb5e551134326c14
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-02-14 15:26:22 +0100

    Added commented out Blx\Plugin\Jb\FixBetaUrl to run.php

--- example/run.php
+++ example/run.php
@@ -4,5 +4,7 @@ $newsRealm = null; // NULL for default
 $appDir = dirname( __FILE__ );
 require 'init.php';
 
+# $request->addPlugin( new Blx\Plugin\Jb\FixBetaUrl() );
+
 # dispatch
 $request->dispatch();
