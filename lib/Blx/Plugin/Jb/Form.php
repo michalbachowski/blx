29a587955639a00b836ed7d46075a5738adfb0f6
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-06-05 15:13:47 +0200

    Handle 'POST' and 'GET' requests that are directed to forms

--- lib/Blx/Plugin/Jb/Form.php
+++ lib/Blx/Plugin/Jb/Form.php
@@ -21,24 +21,28 @@ class DummyForm extends \Jpl_Form_Factory {
 class Form extends \Blx\Plugin {
 
     protected $mapping = array(
-        'filter.response.normal' => 'replace'
+        'filter.response.normal' => 'replace',
+        'handle.get' => 'get',
+        'handle.post' => 'post',
     );
 
     protected $forms = array();
+    protected $callbacks = array();
     protected $view;
-    protected $pattern = '!\[form\:(?P<name>[a-zA-Z0-9]+)\]!';
+    protected $pattern = '!\[form\:(?P<url>[a-zA-Z0-9\\/.\-_]+)\]!';
 
-    public function __construct( $appDir, $name, \Jpl_Form_Factory $obj = null ) {
+    public function __construct( $appDir, $url, $callback, \Jpl_Form_Factory $obj = null ) {
         \Jpl_Form_Factory::setConfigDirectory( 'forms/' );
         \Jpl_Form_Factory::setAppPath( $appDir );
-        $this->setForm( $name, $obj );
+        $this->setForm( $url, $callback, $obj );
     }
 
-    public function setForm( $name, \Jpl_Form_Factory $obj = null ) { 
+    public function setForm( $url, $callback, \Jpl_Form_Factory $obj = null ) { 
         if ( null === $obj || !$obj instanceof \Jpl_Form_Factory ) {
-            $obj = new DummyForm( $name );
+            $obj = new DummyForm( $url );
         }
-        $this->forms[$name] = $obj;
+        $this->forms[$url] = $obj;
+        $this->callbacks[$url] = $callback;
         return $this;
     }
 
@@ -51,15 +55,42 @@ class Form extends \Blx\Plugin {
     }
 
     protected function replaceCallback( $matches ) {
-        $name = $matches['name'];
-        if ( !isset( $this->forms[$name] ) ) {
+        $url = $matches['url'];
+        if ( !isset( $this->forms[$url] ) ) {
             return '';
         }
-        return $this->displayForm( $name );
+        return $this->displayForm( $url );
     }
 
-    public function displayForm( $name ) {
-        return $this->forms[$name]->getForm()->render( $this->view() );
+    public function displayForm( $url ) {
+        return $this->forms[$url]->getForm()->render( $this->view() );
+    }
+    
+    public function get( \sfEvent $event ) {
+        if ( !isset( $this->forms[$event['url']] ) ) {
+            return false;
+        }
+        $event->setReturnValue( $this->displayForm( $event['url'] ) );
+        return true;
+    }
+
+    public function post( \sfEvent $event ) {
+        if ( !isset( $this->forms[$event['url']] ) ) {
+            return false;
+        }
+        $form = $this->forms[$event['url']]->getForm();
+
+        if ( $form->isValid( $event['arguments'] ) ) {
+            $event->setReturnValue( $this->executeCallback( $event ) );
+        } else {
+            $event->setReturnValue( $this->displayForm( $event['url'] ) );
+        }
+        return true;
+    }
+
+    public function executeCallback( \sfEvent $event ) {
+        $func = $this->callbacks[$event['url']];
+        return $func( $event['arguments'], $event );
     }
 
     protected function view() {
