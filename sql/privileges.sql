378d18934257d39f2ded93bb0e9688bf547afce5
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-07-20 10:04:22 +0200

    Fixed privileges

--- sql/privileges.sql
+++ sql/privileges.sql
@@ -7,16 +7,24 @@ GRANT USAGE ON SCHEMA blx TO jaskinia_common;
 
 ALTER FUNCTION list_pages(varchar) OWNER TO jaskinia;
 REVOKE ALL ON FUNCTION list_pages(varchar) FROM PUBLIC;
-GRANT ALL ON FUNCTION list_pages(varchar) TO jaskinia_common;
+REVOKE ALL ON FUNCTION list_pages(varchar) FROM jaskinia_common;
+GRANT EXECUTE ON FUNCTION list_pages(varchar) TO jaskinia_common;
 
 ALTER FUNCTION get_page(varchar, varchar) OWNER TO jaskinia;
 REVOKE ALL ON FUNCTION get_page(varchar, varchar) FROM PUBLIC;
-GRANT ALL ON FUNCTION get_page(varchar, varchar) TO jaskinia_common;
+REVOKE ALL ON FUNCTION get_page(varchar, varchar) FROM jaskinia_common;
+GRANT EXECUTE ON FUNCTION get_page(varchar, varchar) TO jaskinia_common;
 
 ALTER FUNCTION set_page(varchar, varchar, varchar, integer,text, text) owner to jaskinia;
 REVOKE ALL ON FUNCTION set_page(varchar, varchar, varchar, integer, text, text) FROM PUBLIC;
-GRANT ALL ON FUNCTION set_page(varchar, varchar, varchar, integer, text, text) TO jaskinia_common;
+REVOKE ALL ON FUNCTION set_page(varchar, varchar, varchar, integer, text, text) FROM jaskinia_common;
+GRANT EXECUTE ON FUNCTION set_page(varchar, varchar, varchar, integer, text, text) TO jaskinia_common;
 
 alter sequence pages_search_id_seq owner to jaskinia;
 revoke all on sequence pages_search_id_seq from public;
-grant all on sequence pages_search_id_seq to jaskinia_common;
+revoke all on sequence pages_search_id_seq from jaskinia_common;
+grant select, update on sequence pages_search_id_seq to jaskinia_common;
+
+alter function tr_pages_trace() owner to jaskinia;
+revoke all on function tr_pages_trace() from public;
+grant execute on function tr_pages_trace() to jaskinia_common;
